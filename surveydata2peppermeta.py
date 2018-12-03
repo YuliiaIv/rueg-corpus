0d2dc0ee18e83a701ab11212f0a98f9fb689a3cd
Martin Klotz <MartinKl@hu-berlin.de>
2018-12-03 16:19:18 +0100

    extended meta data extraction - extracted some - does not work with Turkish - some codes failed due to typos

--- surveydata2peppermeta.py
+++ surveydata2peppermeta.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentParser
 from collections import defaultdict
 from functools import partial
+from itertools import chain
 import logging
+
 import numpy as np
 import os
 import pandas
@@ -22,6 +24,9 @@ logger = logging.getLogger(__name__)
 raw_data = pandas.read_table(args.survey_results_file, sep=',')
 raw_data['datestamp'] = pandas.to_datetime(raw_data['datestamp'])
 raw_data['dateofbirth'] = pandas.to_datetime(raw_data['dateofbirth'])
+
+
+CODE_PATTERN = r'(D[Ee]|U[Ss]|R[Uu]|G[Rr]|T[Rr])(bi|mo)[0-9][0-9](M|F)(R|G|D|E|T)'
 
 
 def value(data_frame, column_name, astype=None, transform=lambda x: x):
@@ -128,31 +133,75 @@ target_to_extractor = {
     'speaker-personality-score-openness': partial(tipi, score_key='openness'),
     '_project': partial(value, column_name='projectid')
 }
-# TODO the following keys have to be annotated manually or further information is required for encoding
-additional_meta_names = [
-    'formality',  # given in file name
-    'mode',  # given in file name
-    'elicitation-order',  # to be taken from additional file
-    'elicitation-language',  # take from elicitation file name
-    'transcriber-id'  # to be added by transcriber
-    'elicitator-id', # TODO ask for table
-]
 
 data = pandas.DataFrame()
-for name, function in target_to_extractor.items():
-    data[name] = function(raw_data)
+for file_name, function in target_to_extractor.items():
+    data[file_name] = function(raw_data)
 
 out_kv = defaultdict(set)
 for code in data['speaker-id']:
-    if not pandas.isnull(code) and (args.r or re.match(r'(DE|US|RU|GR|TR)(bi|mo)[0-9][0-9](M|F)(R|G|D|E|T)', code)):
-        for name in data:
-            if not name.startswith('_'):
-                val = data[data['speaker-id'] == code][name]
-                out_kv[code].add('='.join((name, str(val.values[0]))))
+    if not pandas.isnull(code) and (args.r or re.match(CODE_PATTERN, code)):
+        for file_name in data:
+            if not file_name.startswith('_'):
+                val = data[data['speaker-id'] == code][file_name]
+                out_kv[code].add('='.join((file_name, str(val.values[0]))))
     else:
         logger.warn('Dropped code {}'.format(code))
+
+
+def name_matches(file_name):
+    return '_' in file_name and re.match(CODE_PATTERN, file_name)
+
+
+all_matching_file_names = filter(name_matches, chain(*(entry[2] for entry in os.walk('.'))))
+suffixes_by_code = defaultdict(set)
+for file_name in all_matching_file_names:
+    code, suffix, _ = re.split(r'_|\.', file_name)
+    suffixes_by_code[code].add(suffix)
+
+INVALID_DIGIT = '<INVALID DIGIT>'
+
+
+def formality_from_suffix(suffix):
+    return 'informal' if suffix[0] == 'i' else ('formal' if suffix[0] == 'f' else INVALID_DIGIT)
+
+
+def mode_from_suffix(suffix):
+    return 'spoken' if suffix[1] == 's' else ('written' if suffix[1] == 'w' else INVALID_DIGIT)
+
+
+
+_LANG_DIGIT_TO_LANG = {
+    'D': 'German',
+    'E': 'English',
+    'G': 'Greek',
+    'R': 'Russian',
+    'T': 'Turkish'
+}
+lang_dict = defaultdict(partial(str, INVALID_DIGIT), _LANG_DIGIT_TO_LANG)
+
+
+def elicitation_lang_from_suffix(suffix):
+    return lang_dict[suffix[2]] if len(suffix) > 2 else INVALID_DIGIT
+
+
+def additional_meta_data(suffix):
+    if len(suffix) < 3:
+        logger.warn('File suffix {} is not a valid suffix (code {})'.format(suffix, code))
+    empty_value = '<NO VALUE>'
+    return [('formality', formality_from_suffix(suffix)),
+            ('mode', mode_from_suffix(suffix)),
+            ('elicitation-order', empty_value),  # to be taken from additional file
+            ('elicitation-language', elicitation_lang_from_suffix(suffix)),  # take from elicitation file name
+            ('transcriber-id', empty_value),  # to be added by transcriber
+            ('elicitator-id', empty_value)]
+
+
 if not os.path.exists(args.target_dir):
     os.mkdir(args.target_dir)
-for file_name, metadata in out_kv.items():
-    with open(os.path.join(args.target_dir, '{}.meta'.format(file_name)), 'w') as f:
-        f.write(os.linesep.join(sorted(metadata) + ['']))
+for file_code, metadata in out_kv.items():
+    for sfx in suffixes_by_code[file_code]:
+        with open(os.path.join(args.target_dir, '{}_{}.meta'.format(file_code, sfx)), 'w') as f:
+            f.write(os.linesep.join(sorted(metadata) + \
+                                    ['='.join((k, v)) for k, v in additional_meta_data(sfx)] + \
+                                    ['']))
