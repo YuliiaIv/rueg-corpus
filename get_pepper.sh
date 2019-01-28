2f3da61aff2aafea3721d6444ee03d38f106bd18
Martin Klotz <MartinKl@hu-berlin.de>
2019-01-28 18:23:15 +0100

    modified pepper script

--- get_pepper.sh
+++ get_pepper.sh
@@ -5,28 +5,38 @@ if [[ -z $1 ]]; then
 	echo "Error: No module workspace provided."
 	exit
 fi
-corpushome=$pwd
-exbmodulepath="$1""pepperModules-EXMARaLDAModules/target/"
-ruegmodulepath="pepper.dropin.paths=""$1""pepperModules-RUEGModules/target/"
-textgridmodulepath="$1""pepperModules-TextGridModules/target/"
+corpushome=$(pwd)
+echo $corpushome
+exbmodulepath="$1""pepperModules-EXMARaLDAModules/"
+ruegmodulepath="$1""pepperModules-RUEGModules/"
+textgridmodulepath="$1""pepperModules-TextGridModules/"
 curl -X GET https://korpling.german.hu-berlin.de/saltnpepper/pepper/download/snapshot/Pepper_2018.12.20-SNAPSHOT.zip --output pepper.zip
 unzip pepper.zip
 cd pepper/
-echo "$ruegmodulepath"",""$exbmodulepath"",""$textgridmodulepath" >> conf/pepper.properties
+configdropins="$ruegmodulepath""target/,""$exbmodulepath""target/,""$textgridmodulepath""target"
+echo $configdropins
+echo "" >> conf/pepper.properties
+echo "pepper.dropin.paths=""$configdropins" >> conf/pepper.properties
 cd ..
 rm pepper.zip
 cd "$1"
-if [[ ! -e "$exbmodulepath" ]]:
+modulehome=$(pwd)
+if [[ ! -d "$exbmodulepath" ]]; then
 	git clone git@github.com:korpling/pepperModules-EXMARaLDAModules -b develop
-cd $exbmodulepath$
-mvn clean install
-if [[ ! -e "$ruegmodulepath" ]]:
+fi
+cd $exbmodulepath
+mvn clean install -DskipTests
+cd $modulehome
+if [[ ! -d "$ruegmodulepath" ]]; then
 	git clone git@github.com:korpling/pepperModules-RUEGModules -b develop
-cd $ruegmodulepath$
-mvn clean install
-if [[ ! -e "$textgridmodulepath" ]]:
+fi
+cd $ruegmodulepath
+mvn clean install -DskipTests
+cd $modulehome
+if [[ ! -d "$textgridmodulepath" ]]; then
 	git clone git@github.com:korpling/pepperModules-TextGridModules -b develop
-cd $textgridmodulepath$
-mvn clean install
+fi
+cd $textgridmodulepath
+mvn clean install -DskipTests
 cd "$corpushome"
 echo "pepper installed successfully!"
